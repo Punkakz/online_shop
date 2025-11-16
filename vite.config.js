45f6517aeb1e72ddfac7f20f71ba56b6e6969e4c
Ubuntu <ubuntu@ip-172-31-25-200.ec2.internal>
2025-11-16 14:34:18 +0000

    back to it was

--- vite.config.js
+++ vite.config.js
@@ -12,8 +12,11 @@ export default defineConfig({
       port: 3000,
       strictPort: true,
       allowedHosts: [
-        '16-16-128-178.nip.io',
-        'localhost'
-      ]
+	  '16-16-128-178.nip.io',
+	  'localhost',
+	  'akhtar-ansari.site',
+	  'www.akhtar-ansari.site'
+]
+
     }
 })
